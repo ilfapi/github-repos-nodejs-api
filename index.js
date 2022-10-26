4b4f718d4739fb8216a2458d8cc635bbec98eabd
ilfapi <116699810+ilfapi@users.noreply.github.com>
2022-10-26 10:38:38 +0700

    Update index.js

--- index.js
+++ index.js
@@ -3,25 +3,8 @@ const axios = require('axios');
 const app = express();
 const PORT = process.env.PORT || 8081;
 
-app.get('/', async (req, res) => {
-  const username = req.query.username || 'myogeshchavan97';
-  try {
-    const result = await axios.get(
-      `https://api.github.com/users/${username}/repos`
-    );
-    const repos = result.data
-      .map((repo) => ({
-        name: repo.name,
-        url: repo.html_url,
-        description: repo.description,
-        stars: repo.stargazers_count
-      }))
-      .sort((a, b) => b.stars - a.stars);
-
-    res.send(repos);
-  } catch (error) {
-    res.status(400).send('Error while getting list of repositories');
-  }
+app.get('/', (req, res) => {
+  res.status(200).send(`API is built by ILF Football</br>`);
 });
 
 app.listen(PORT, () => {
