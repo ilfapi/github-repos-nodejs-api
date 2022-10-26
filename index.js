500dc39ccb8b78ca74436f4da36a445d5faf3c48
ilfapi <116699810+ilfapi@users.noreply.github.com>
2022-10-26 12:30:34 +0700

    add cors to index

--- index.js
+++ index.js
@@ -1,15 +1,21 @@
 const express = require('express');
-const cors = require('cors');
+// const cors = require('cors');
 const axios = require('axios');
 const app = express();
 const PORT = process.env.PORT || 8081;
-app.use(cors());
+// app.use(cors());
 
 app.get('/', (req, res) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+  res.header('Access-Control-Allow-Headers', 'X-Requested-With, Accept, Origin, Referer, User-Agent, Content-Type, Authorization');
   res.status(200).send(`API is built by ILF Football</br>`);
 });
 
 app.get('/api', (req, res) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+  res.header('Access-Control-Allow-Headers', 'X-Requested-With, Accept, Origin, Referer, User-Agent, Content-Type, Authorization');
   var config = {
     method: 'get',
     url: 'https://api.football-data.org/v4/competitions/PL/',
